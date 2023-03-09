a55924ed09a4bbf427638535138e3fd509b127fe
Ismail K <ismailkoormath037@gmail.com>
2023-03-09 19:46:38 +0530

    detail controller creared

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Query } from "mongoose";
 import Property from "../mongodb/models/property.js";
 import User from "../mongodb/models/user.js";
 import * as dotenv from 'dotenv'
@@ -12,8 +12,48 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-const getAllProperties = async (req, res) => {};
-const getPropertyDetail = async (req, res) => {};
+//git all properties
+
+const getAllProperties = async (req, res) => {
+  const {_end, _order, _start, _sort, title_like = "",propertyType=""}=req.query
+  const query = {}
+  if(propertyType !==''){
+    query.propertyType=propertyType
+  }
+
+  if(title_like){
+    query.title = {$regex : title_like, $options:'i'}
+  }
+  try {
+    const count = await Property.countDocuments({query})
+
+    const properties = await Property
+    .find(query)
+    .limit(_end)
+    .skip(_start)
+    .sort({ [_sort]: _order})
+
+    res.header('x-total-count', count)
+    res.header('Access-Control-Expose-Headers',
+    'x-total-count')
+
+    res.status(200).json(properties)
+  } catch (error) {
+    res.status(500).json({ message:error.message})
+  }
+}
+
+
+const getPropertyDetail = async (req, res) => {
+const {id} = req.params
+const propertyExists = await Property.findOne({_id: id}).populate('creator')
+if(propertyExists){
+  res.status(200).json(propertyExists)
+}else{
+  res.status(404).json({ message:'Property not found'})
+}
+}
+//Create properties
 
 const createProperty = async (req, res) => {
   try{
